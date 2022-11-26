9d439655a06f13f728c539f5226275e1178c68eb
Oğuzhan Küçükyamaç <okucukyamac@outlook.com>
2022-11-26 09:16:17 +0300

    cmt

---
+++ BlogApp/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.Models;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BlogApp.ViewComponents.Comment
+{
+    public class CommentListByBlog: ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetAll(13);
+            return View(values);
+        }
+    }
+}
