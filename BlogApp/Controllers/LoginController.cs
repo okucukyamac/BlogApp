076a9cb128275a2a2cce79091e7005c5e6463032
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-01-22 23:05:42 +0300

    startup authentication

--- BlogApp/Controllers/LoginController.cs
+++ BlogApp/Controllers/LoginController.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccess.Concrete;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BlogApp.Controllers
 {
-    public class LoginController : Controller
-    {
-        [AllowAnonymous]
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	[AllowAnonymous]
+	public class LoginController : Controller
+	{		
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(Writer model)
+		{
+			Context c = new();
+			var dataValue = c.Writers.FirstOrDefault(x => x.Mail == model.Mail && x.Password == model.Password);
+			if (dataValue != null)
+			{
+				HttpContext.Session.SetString("username", model.Mail);
+				return RedirectToAction("Index", "Writer");
+			}
+			else
+				return View();
+		}
+	}
 }
--- BlogApp/Startup.cs
+++ BlogApp/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -13,58 +14,69 @@ using System.Threading.Tasks;
 
 namespace BlogApp
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews();
 
-            services.AddMvc(config =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                config.Filters.Add(new AuthorizeFilter(policy));
-            });
-        }
+			services.AddSession();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			services.AddMvc(config =>
+			{
+				var policy = new AuthorizationPolicyBuilder()
+					.RequireAuthenticatedUser()
+					.Build();
+				config.Filters.Add(new AuthorizeFilter(policy));
+			});
 
-            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code={0}");
+			services.AddMvc();
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie(x =>
+				{
+					x.LoginPath = "/Login/Index";
+				});
+		}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseRouting();
+			app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code={0}");
 
-            app.UseAuthorization();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseSession();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
